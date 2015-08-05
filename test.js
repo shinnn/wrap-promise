7d233554e1e3c8832fc618502591358a9f12ae3e
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2015-08-05 10:23:01 +0900

    write test with ES2015 syntax

--- test.js
+++ test.js
@@ -1,16 +1,16 @@
 'use strict';
 
-var fs = require('fs');
-var path = require('path');
+const fs = require('fs');
+const path = require('path');
 
-var es6Promise = require('es6-promise').Promise;
-var requireUncached = require('require-uncached');
-var test = require('tape');
+const es6Promise = require('es6-promise').Promise;
+const requireUncached = require('require-uncached');
+const test = require('tape');
 
-var bowerMainPath = path.resolve(require('./bower.json').main);
+const bowerMainPath = path.resolve(require('./bower.json').main);
 
 function runTest(description, requireWrapPromise, supported) {
-  test(description, function(t) {
+  test(description, t => {
     if (!supported) {
       t.throws(
         requireWrapPromise,
@@ -23,10 +23,10 @@ function runTest(description, requireWrapPromise, supported) {
 
     t.plan(6);
 
-    var wrapPromise = requireWrapPromise();
+    const wrapPromise = requireWrapPromise();
     t.equal(wrapPromise.name, 'wrapPromise', 'should have a function name.');
 
-    var readFile = function(filePath) {
+    function readFile(filePath) {
       return wrapPromise(function(resolve, reject) {
         fs.readFile(filePath, function(err, buf) {
           if (err) {
@@ -36,68 +36,68 @@ function runTest(description, requireWrapPromise, supported) {
           resolve(buf);
         });
       });
-    };
+    }
 
-    readFile('.gitattributes').then(function(buf) {
+    readFile('.gitattributes').then(buf => {
       t.equal(
-        buf.toString(),
+        String(buf),
         '* text=auto\n',
         'should provide .then() method to the returned object.'
       );
-    });
+    }).catch(t.fail);
 
-    readFile('__this_file_does_not_exists__').catch(function(err) {
+    readFile('__this_file_does_not_exists__').catch(err => {
       t.equal(err.code, 'ENOENT', 'should provide .catch() method to the returned object.');
-    });
+    }).catch(t.fail);
 
     t.throws(
-      readFile.bind(null, [__dirname]),
+      () => readFile([__dirname]),
       /TypeError.*path/,
       'should immediately throw an error when an error is thrown in the function.'
     );
 
     t.throws(
-      wrapPromise.bind(null, {'this': {is: {not: {a: {'function': '.'}}}}}),
+      () => wrapPromise({This: {is: {not: {a: {function: '.'}}}}}),
       /TypeError.*must be a function/,
       'should throw a type error when the argument is not a function.'
     );
 
     t.throws(
-      wrapPromise.bind(null),
+      () => wrapPromise(),
       /TypeError.*must be a function/,
       'should throw a type error when it takes no arguments.'
     );
   });
 }
 
-var env = ' without global.Promise';
+let env = ' without global.Promise';
 
-runTest('require(\'wrap-promise\')' + env, function() {
+runTest('require(\'wrap-promise\')' + env, () => {
   global.Promise = null;
   return requireUncached('./');
 }, true);
-runTest('require(\'wrap-promise/no-fallback\')' + env, function() {
+runTest('require(\'wrap-promise/no-fallback\')' + env, () => {
   global.Promise = null;
   return requireUncached('./no-fallback');
 }, false);
 
 env = ' with global.Promise';
 
-runTest('require(\'wrap-promise\')' + env, function() {
+runTest('require(\'wrap-promise\')' + env, () => {
   global.Promise = es6Promise;
   return require('./');
 }, true);
-runTest('require(\'wrap-promise/no-fallback\')' + env, function() {
+runTest('require(\'wrap-promise/no-fallback\')' + env, () => {
   global.Promise = es6Promise;
   return require('./no-fallback');
 }, true);
 
-runTest('window.wrapPromise without window.Promise', function() {
+runTest('window.wrapPromise without window.Promise', () => {
   global.window = {};
   requireUncached(bowerMainPath);
 }, false);
 
-runTest('window.wrapPromise with window.Promise', function() {
+runTest('window.wrapPromise with window.Promise', () => {
   global.window = {Promise: es6Promise};
   requireUncached(bowerMainPath);
   return window.wrapPromise;
